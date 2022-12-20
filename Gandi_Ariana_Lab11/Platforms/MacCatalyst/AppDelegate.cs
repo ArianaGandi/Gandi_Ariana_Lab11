5e915c41f8ada4b3904226416eaa5049cddce867
Ariana Gandi <ary_gandi@yahoo.com>
2022-12-20 15:50:24 +0200

    Add project files.

---
+++ Gandi_Ariana_Lab11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Gandi_Ariana_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
